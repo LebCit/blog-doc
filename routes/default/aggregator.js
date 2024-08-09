7aee7278d8e2ee2f9d7ea2b6b2dca061c6462dc9
LebCit <lebcit@gmail.com>
2024-08-09 08:02:33 +0300

    Combine default theme routes

---
+++ routes/default/aggregator.js
@@ -0,0 +1,15 @@
+import { settings } from "../../config/settings.js"
+import { app } from "../../functions/initialize.js"
+
+// default theme routes
+import { mainRoute } from "./mainRoute.js"
+import { archiveRoute } from "./archiveRoute.js"
+import { markdownRoute } from "./markdownRoute.js"
+import { tagsRoute } from "./tagsRoute.js"
+import { rssRoute } from "./rssRoute.js"
+import { sitemapRoute } from "./sitemapRoute.js"
+import { searchRoute } from "./searchRoute.js"
+
+const routes = [mainRoute, archiveRoute, markdownRoute, tagsRoute, rssRoute, sitemapRoute, searchRoute]
+
+routes.forEach((route) => route(app, settings))
