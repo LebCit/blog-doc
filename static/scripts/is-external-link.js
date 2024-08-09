6977919ace5b5c753bd2a9baea0bff52fbc02787
LebCit <lebcit@gmail.com>
2024-08-09 09:21:59 +0300

    Remove unneeded scripts from the `scripts` folder in the `static` directory

--- static/scripts/is-external-link.js
+++
@@ -1,7 +0,0 @@
-// Function to check if a link is external
-export function isExternalLink(href) {
-	const currentDomain = window.location.host // .host includes the port number if present
-	const link = document.createElement("a")
-	link.href = href
-	return link.host !== currentDomain
-}
