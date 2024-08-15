ac09a3fb8b748bba2640edb1a98b6fad1a0fabb3
LebCit <lebcit@gmail.com>
2024-08-16 02:27:38 +0300

    Load global rss and sitemap routes

--- routes/default/aggregator.js
+++ routes/default/aggregator.js
@@ -6,8 +6,8 @@ import { mainRoute } from "./mainRoute.js"
 import { archiveRoute } from "./archiveRoute.js"
 import { markdownRoute } from "./markdownRoute.js"
 import { tagsRoute } from "./tagsRoute.js"
-import { rssRoute } from "./rssRoute.js"
-import { sitemapRoute } from "./sitemapRoute.js"
+import { rssRoute } from "../rssRoute.js"
+import { sitemapRoute } from "../sitemapRoute.js"
 import { searchRoute } from "./searchRoute.js"
 
 const routes = [mainRoute, archiveRoute, markdownRoute, tagsRoute, rssRoute, sitemapRoute, searchRoute]
