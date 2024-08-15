822efc9846b907668490858c385bcbf024d4a55e
LebCit <lebcit@gmail.com>
2024-08-16 02:12:08 +0300

    Refactor markdown post processing for improved readability and maintainability

--- functions/helpers/processMarkdownPosts.js
+++ functions/helpers/processMarkdownPosts.js
@@ -1,35 +1,42 @@
+import { marked } from "marked"
+import { settings } from "../../config/settings.js"
+
 /**
  * Processes files from the posts directory, adds post excerpts, sorts by publish date with previous and next references.
  * @param {Object} app - LiteNode's instance.
  * @returns {Promise<Array<Object>>} - A promise resolving to an array of processed files objects.
  */
 export async function processMarkdownPosts(app) {
+	const { postPreviewFallbackImage } = settings
+
 	// Fetch and process the files
 	const posts = (await app.parseMarkdownFileS("posts"))
-		.filter((post) => post.frontmatter.published === true)
+		.filter(({ frontmatter }) => frontmatter.published)
 		.map((post) => {
-			// Add postExcerpt to posts
 			const excerptLength = 180
-			post.postExcerpt =
-				post.content.length <= excerptLength ? post.content : `${post.content.substring(0, excerptLength)}...`
+			const { content, frontmatter } = post
 
-			// Add pubDate to each post
-			post.pubDate = new Date(post.frontmatter.publish_date)
+			post.postExcerpt = marked.parse(
+				content.length <= excerptLength ? content : `${content.substring(0, excerptLength)}...`
+			)
+
+			frontmatter.featuredImage = frontmatter.featuredImage || postPreviewFallbackImage
+			frontmatter.description = frontmatter.description || ""
+
+			post.pubDate = new Date(frontmatter.publish_date)
 
 			return post
 		})
+		.sort((a, b) => new Date(b.frontmatter.publish_date) - new Date(a.frontmatter.publish_date))
 
-	// Sort the posts by publish_date from newer to older
-	posts.sort((a, b) => {
-		const dateA = new Date(a.frontmatter.publish_date).getTime()
-		const dateB = new Date(b.frontmatter.publish_date).getTime()
-		return dateB - dateA
-	})
-
-	// Add previous and next references
+	// Add lightweight references to the previous and next posts
 	posts.forEach((post, index) => {
-		post.nextPost = index > 0 ? posts[index - 1] : null
-		post.prevPost = index < posts.length - 1 ? posts[index + 1] : null
+		const prevPost = posts[index + 1] || null
+		const nextPost = posts[index - 1] || null
+
+		post.prevPost = prevPost ? { title: prevPost.frontmatter.title, fileBaseName: prevPost.fileBaseName } : null
+
+		post.nextPost = nextPost ? { title: nextPost.frontmatter.title, fileBaseName: nextPost.fileBaseName } : null
 	})
 
 	return posts
