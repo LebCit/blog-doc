8a38988bc706d4cfc4285f0df13538132b000484
LebCit <lebcit@gmail.com>
2024-08-16 02:01:42 +0300

    Remove Eta from initialization file

--- functions/initialize.js
+++ functions/initialize.js
@@ -1,8 +1,5 @@
-import { join } from "path"
 import { LiteNode } from "litenode"
-import { Eta } from "eta"
 
 const app = new LiteNode()
-const eta = new Eta({ views: join(process.cwd(), "views") })
 
-export { app, eta }
+export { app }
