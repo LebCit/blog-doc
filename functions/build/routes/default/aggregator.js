2d3e366e5d9300e5c40e0857832357e8327b4088
LebCit <lebcit@gmail.com>
2024-08-17 23:34:15 +0300

    Update themes routes aggregators for build to use global not found, rss and sitemap routes

--- functions/build/routes/default/aggregator.js
+++ functions/build/routes/default/aggregator.js
@@ -7,10 +7,11 @@ import { archiveRoute } from "./archiveRoute.js"
 import { markdownRoute } from "./markdownRoute.js"
 import { tagRoute } from "./tagRoute.js"
 import { tagsRoute } from "./tagsRoute.js"
-import { rssRoute } from "./rssRoute.js"
-import { sitemapRoute } from "./sitemapRoute.js"
+import { rssRoute } from "../rssRoute.js"
+import { sitemapRoute } from "../sitemapRoute.js"
+import { notFoundRoute } from "../notFoundRoute.js"
 //import { searchRoute } from "./searchRoute.js"
 
-const routes = [mainRoute, archiveRoute, markdownRoute, tagRoute, tagsRoute, rssRoute, sitemapRoute]
+const routes = [mainRoute, archiveRoute, markdownRoute, tagRoute, tagsRoute, rssRoute, sitemapRoute, notFoundRoute]
 
 routes.forEach((route) => route(app, settings))
